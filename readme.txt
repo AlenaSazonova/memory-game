a93ee52dfdd78ed889849709ac945edd9dcc870a
AlenaSazonova <alenka03.92@mail.ru>
2023-12-07 15:27:47 +0300

    feat: add work cards

---
+++ readme.txt
@@ -0,0 +1,9 @@
+Technical specification
+
+Core concept: 
+The game is a set of identical cards. The goal of the game is to reveal cards in pairs. 
+When the cards match, they are removed.The task is to completely clear the playing field.
+
+At this stage the game has implemented:
+Cards rendered to screen with Java Script. The cards are first turned over to the side with the picture, 
+and when pressed again, it return to original state. The card turns over with an animation effect
--- scripts/objects.js
+++ scripts/objects.js
@@ -1,4 +1,53 @@
 'use strict';
 
-const menu = document.querySelector('.menu');
+const cards = [
+    {id: 1, title: 'Cinderella', img: '/assets/Cinderella.PNG', imgBack: '/assets/backSide.PNG'},
+    {id: 2, title: 'Elsa', img: '/assets/Elsa.PNG', imgBack: '/assets/backSide.PNG'},
+    {id: 3, title: 'Mermaid', img: '/assets/Mermaid.PNG', imgBack: '/assets/backSide.PNG'},
+    {id: 4, title: 'SnowWhite', img: '/assets/SnowWhite.PNG', imgBack: '/assets/backSide.PNG'}
+]
+
+function renderHandler() {
+    cards.map((el) => templateImg(el['img'], el['id'], el['imgBack'])).join('');
+}
+renderHandler()
+
+
+function templateImg(img, id, imgBack) {
+    const col = document.createElement('div');
+    col.classList.add('col');
+    col.insertAdjacentHTML('afterbegin', `
+        <div class="card">
+            <img class="card-img card-back off" data-side="back" data-id=${id} src=${imgBack}>
+            <img class="card-img card-front on" data-side="front" data-id=${id} src=${img}>
+        </div>
+        <div class="card">
+            <img class="card-img card-back off" data-side="back" data-id=${id} src=${imgBack}>
+            <img class="card-img card-front on" data-side="front" data-id=${id} src=${img}>
+        </div>
+    `)
+
+    const menu = document.querySelector('.menu');
+    menu.append(col);
+}
+
+
+let allCards = document.querySelectorAll('.card');
+
+
+function turnOverCard(event) {
+    const cardSide = event.currentTarget; 
+    const id = event.target.dataset.id;
+
+    if (!cardSide.classList.contains('on')) {
+        cardSide.classList.remove('off');
+		cardSide.classList.add('on');
+	} else if (cardSide.classList.contains('on')) {
+		cardSide.classList.remove('on');
+		cardSide.classList.add('off');
+	}
+}
+
+
+allCards.forEach((card) => card.addEventListener('click', turnOverCard));
 
